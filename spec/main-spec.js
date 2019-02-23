a785092f7d0f2056cd61097a05b5340e8e086ed8
facccge <fcfc5702@qq.com>
2019-02-23 23:04:44 +0800

    test function loadAllItems and loadPromotions

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,6 +1,9 @@
 const main = require('../main/main');
+const datbase = require('../main/datbase');
 const printInventory = main.printInventory;
 const formateInputs = main.formateInputs;
+const loadAllItems = datbase.loadAllItems
+const loadPromotions = datbase.loadPromotions
 
 describe('pos', function () {
     var inputs;
@@ -40,6 +43,68 @@ describe('pos', function () {
             '**********************';
 
         expect(console.log).toHaveBeenCalledWith(expectText);
+    });
+});
+
+describe('loadAllItems', function () {
+    it('should return all items', function () {
+        let result = loadAllItems();
+        let expected=[
+            {
+                barcode: 'ITEM000000',
+                name: '可口可乐',
+                unit: '瓶',
+                price: 3.00
+            },
+            {
+                barcode: 'ITEM000001',
+                name: '雪碧',
+                unit: '瓶',
+                price: 3.00
+            },
+            {
+                barcode: 'ITEM000002',
+                name: '苹果',
+                unit: '斤',
+                price: 5.50
+            },
+            {
+                barcode: 'ITEM000003',
+                name: '荔枝',
+                unit: '斤',
+                price: 15.00
+            },
+            {
+                barcode: 'ITEM000004',
+                name: '电池',
+                unit: '个',
+                price: 2.00
+            },
+            {
+                barcode: 'ITEM000005',
+                name: '方便面',
+                unit: '袋',
+                price: 4.50
+            }
+        ];
+        expect(result).toEqual(expected)
+    });
+});
+
+describe('loadPromotions', function () {
+    it('should return all promotions', function () {
+        let result = loadPromotions();
+        let expected=[
+            {
+                type: 'BUY_TWO_GET_ONE_FREE',
+                barcodes: [
+                    'ITEM000000',
+                    'ITEM000001',
+                    'ITEM000005'
+                ]
+            }
+        ];
+        expect(result).toEqual(expected)
     });
 });
 
